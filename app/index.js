1f8742dc5144f4c345d903e024e8ce85a8cd9a75
arbesakajtazi <arbesakajtazi@odysseyprime.nl>
2019-10-15 14:03:42 +0200

    initial commit

---
+++ app/index.js
@@ -0,0 +1,10 @@
+/**
+ * Created by ArbesaKajtazi on 14/10/2019.
+ */
+import ReactDOM from 'react-dom'
+import React from 'react'
+import App from "./App";
+
+ReactDOM.render(<App/>, document.getElementById('root'))
+
+
---
+++ webpack.config.js
@@ -0,0 +1,62 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const path = require('path')
+const APP_DIR = path.resolve(__dirname, 'app')
+const BUILD_DIR = path.resolve(__dirname, 'build')
+
+const BUILD_ENV = process.env.NODE_ENV || 'development'
+
+module.exports = {
+  mode: BUILD_ENV,
+  entry: APP_DIR + '/index.js',
+  output: {
+    path: BUILD_DIR,
+    filename: 'bundle.js',
+    publicPath: '/'
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+    modules: [
+      'node_modules',
+      path.resolve(APP_DIR)
+    ]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        },
+      },
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.html$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: "html-loader"
+          }
+        ]
+      },
+      {
+        test: /\.(png|jpg|gif)$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: "file-loader"
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [new HtmlWebpackPlugin({
+    hash: true,
+    template:`${APP_DIR}/index.html`,
+    filename: `${BUILD_DIR}/index.html`
+  })]
+}
